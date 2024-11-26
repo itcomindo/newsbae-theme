b16b8526d8bf9eacfd5367240b57d379597cb871
Budi Haryono <mail.budiharyono@gmail.com>
2024-11-26 09:55:55 +0700

    ok

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,72 +1,87 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    jQuery(function () {
-        //JQuery start below.
+jQuery(function () {
+    // Move Search to Nav
+    function moveSearchToNav() {
+        var $screenWidth = jQuery(window).width();
+        var $search = jQuery('#topbar .mid');
+        var $nav = jQuery('#header-menu nav');
+        var $items = jQuery('#topbar .items');
+        var $left = $items.find('.left');
+        var $right = $items.find('.right');
 
-        // make body overflow hidden start.
-        function makeBodyOverflowHidden() {
-            jQuery('body').addClass('no-scroll');
-            setTimeout(function () {
-                jQuery('body').removeClass('no-scroll');
-            }, 1000);
-        }
-        makeBodyOverflowHidden();
-        // make body overflow hidden end.
-
-        // OffCanvasMenu Start.
-        function offCanvasMenu() {
-            var $toggle = jQuery('.bars.toggle');
-            var $menu = jQuery('nav.header-menu');
-            var $bar = jQuery('.bar');
-
-            $toggle.on('click', function (e) {
-                e.stopPropagation();
-                $menu.toggleClass('active');
-                $bar.toggleClass('active');
-
-                if ($menu.hasClass('active')) {
-                    setTimeout(function () {
-                        if (!$menu.find('.closer').length) { // Pastikan hanya ada satu .closer
-                            $menu.append('<div class="closer">X</div>');
-                        }
-                    }, 800); // Delay 300ms
+        if ($search.length && $nav.length && $items.length) {
+            if ($screenWidth < 769) {
+                $search.prependTo($nav);
+                $search.addClass('in-nav');
+            } else {
+                if ($left.length && $right.length) {
+                    $search.insertAfter($left);
                 } else {
-                    jQuery('.closer').remove();
+                    $search.appendTo($items);
                 }
-            });
-
-
-            jQuery('#adsafhm').slideUp(300, function () {
-                console.log('Ads closed');
-            });
-
-
-
-
-
-            // Klik di mana saja untuk menutup menu
-            jQuery(document).on('click', function () {
-                $menu.removeClass('active');
-                $bar.removeClass('active');
-                jQuery('.closer').remove();
-            });
-
-            // Klik pada .closer untuk menutup menu
-            jQuery(document).on('click', '.closer', function () {
-                $menu.removeClass('active');
-                $bar.removeClass('active');
-                jQuery(this).remove();
-            });
+                $search.removeClass('in-nav');
+            }
         }
+    }
 
-        offCanvasMenu();
+    // Debounce function
+    function debounce(func, wait) {
+        var timeout;
+        return function () {
+            var context = this, args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+                func.apply(context, args);
+            }, wait);
+        };
+    }
 
-        // OffCanvasMenu End.
+    // Make Body Overflow Hidden
+    function makeBodyOverflowHidden() {
+        jQuery('body').addClass('no-scroll');
+        setTimeout(function () {
+            jQuery('body').removeClass('no-scroll');
+        }, 1000);
+    }
 
+    // OffCanvas Menu
+    function offCanvasMenu() {
+        var $toggle = jQuery('.bars.toggle');
+        var $menu = jQuery('nav.header-menu');
+        var $bar = jQuery('.bar');
 
+        $toggle.on('click', function (e) {
+            e.stopPropagation();
+            $menu.toggleClass('active');
+            $bar.toggleClass('active');
 
+            if ($menu.hasClass('active')) {
+                if (!$menu.find('.closer').length) {
+                    $menu.append('<div class="closer">X</div>');
+                }
+            } else {
+                jQuery('.closer').remove();
+            }
+        });
 
+        jQuery(document).on('click', function () {
+            $menu.removeClass('active');
+            $bar.removeClass('active');
+            jQuery('.closer').remove();
+        });
 
+        jQuery(document).on('click', '.closer', function (e) {
+            e.stopPropagation();
+            $menu.removeClass('active');
+            $bar.removeClass('active');
+            jQuery(this).remove();
+        });
+    }
 
-        //JQuery end above.
-    });
+    // Inisialisasi semua fungsi
+    moveSearchToNav();
+    makeBodyOverflowHidden();
+    offCanvasMenu();
+
+    // Optimasi resize
+    jQuery(window).resize(debounce(moveSearchToNav, 200));
 });
